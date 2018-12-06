02b7d7d26f1185e45479ac76cee22f04077f95f3
HimekaidouHatate <HimekaidouHatate@users.noreply.github.com>
2018-12-06 23:38:44 +0900

    불(타는것)

--- scripts/prefabs/sendi_rapier.lua
+++ scripts/prefabs/sendi_rapier.lua
@@ -10,6 +10,10 @@ local assets ={
    Asset("IMAGE", "images/inventoryimages/sendi_rapier.tex"),
 }
 
+local prefabs = {
+	"firesplash_fx", --YUKARI : 이펙트 같은 외부 파일들을 로드해야할땐 반드시 prefabs 어규먼트를 넣어주세요.
+}
+
 local function UpdateDamage(inst)
     if inst.components.perishable and inst.components.weapon then
         local dmg = TUNING.HAMBAT_DAMAGE * inst.components.perishable:GetPercent()
@@ -23,21 +27,14 @@ local function OnLoad(inst, data)
 end
             --onunequip
 local function onequip(inst, owner)
-
-   
     owner.AnimState:OverrideSymbol("swap_object", "swap_sendi_rapier_01", "swap")
-									--"?? " "네임" "애니메이션 경로"
     owner.AnimState:Show("ARM_carry")
     owner.AnimState:Hide("ARM_normal")
-	
 	-- 장착 시 설정.
 	-- owner.AnimState:OverrideSymbol("애니메이션 뱅크명", "빌드명", "빌드 폴더명")
 	-- 그 아래 2줄은 물건을 들고 있는 팔 모습을 활성화하고, 빈 팔 모습을 비활성화.
+end
 
-
-   end
-
-   
 local function onunequip(inst, owner)
     --UpdateDamage(inst)
     owner.AnimState:Hide("ARM_carry")
@@ -48,29 +45,48 @@ local function onunequip(inst, owner)
    -- end
 end
 
+local DEFAULTBURNTIME = 3
+local BURNDAMAGE = 3
+local BURNADDTIME = 2
+local BURNPERIOD = 1
 
 local function onattack(inst, attacker, target)
-   local fx = SpawnPrefab("firesplash_fx")
-      fx.Transform:SetScale(0.5, 0.5, 0.5)
-      fx.Transform:SetPosition(target:GetPosition():Get())
-   if target.components.burnable then
-   target.components.burnable:Ignite()
-   end
-end--MH불꽃데미지
+	local fx = SpawnPrefab("firesplash_fx")
+	fx.Transform:SetScale(0.5, 0.5, 0.5)
+	fx.Transform:SetPosition(target:GetPosition():Get())
+	if target.DotDamageTask ~= nil then 
+		target.burntime = target.burntime + BURNADDTIME
+	else 
+		local BurnTime = target.components.burnable ~= nil and target.components.burnable.burntime -- 해당 개체의 불탈 시간이 존재한다면 그 값을 가져옴
+		local function OnIgnite(target)
+			target.AnimState:SetMultColour(0.8, 0.5, 0.5, 1)
+			if target.components.health ~= nil then
+				target.components.health:DoDelta(-BURNDAMAGE)
+			end
+		end
+		OnIgnite(target)
+		target.burntime = BurnTime ~= nil and (BurnTime / 3) or DEFAULTBURNTIME -- BurnTime이 nil이면 3 대입 (불타던 시간 / 3 만큼 타거나, 3초동안 탐)
+		target.DotDamageTask = target.DoPeriodicTask(target, BURNPERIOD, function() -- 함수를 실행하고 그 참조값을 해당 개체에 남긴다. (외부에서 참조할 수 있게됨)
+			if target.burntime > 0 then
+				OnIgnite(target)
+				target.burntime = target.burntime - 1
+			else
+				target.AnimState:SetMultColour(1, 1, 1, 1) --원래 색깔로
+				target.burntime = 0 -- burntime은 위의 연산에서 음수일 수도 있음
+			end
+		end)
+	end -- Yukari : 커스텀 도트데미지 함수 작성함.
+end
 
 
 local function fn()
 
-    local inst = CreateEntity()
-
-   
-   local inst = CreateEntity()
-    local trans = inst.entity:AddTransform()
-    local anim = inst.entity:AddAnimState()
-   -- 아기쥐 추가   
-   
+	local inst = CreateEntity()  
+	-- local trans = inst.entity:AddTransform() <<<<<<<<<< YUKARI : 이거와 같은 경우에, trans라는 변수가 더이상 쓰이지 않을것 같을땐 변수로 할당하지 않는 습관을 들여주세요.(메모리 낭비됨)
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
     inst.entity:AddNetwork()
- 
+
     MakeInventoryPhysics(inst)
 
 
@@ -94,7 +110,6 @@ local function fn()
     inst:AddComponent("weapon")
     inst.components.weapon:SetDamage(50)
    -- 무기로 설정. 아래는 피해 설정
-    inst.OnLoad = OnLoad
 	inst.components.weapon:SetRange(1.2)
 	--공격범위
    
@@ -116,7 +131,11 @@ local function fn()
 	--장착 가능하도록, 장착밑 해제시의 위의 두 펑션을 작동
 	
 	inst.components.weapon:SetOnAttack(onattack)--MH불꽃데미지 
+
+	inst.OnLoad = OnLoad
+	--YUKARI : OnLoad, OnSave, OnPreLoad 함수들은 마지막에 입력해주세요. 
+
     return inst
 end
 
-return Prefab("sendi_rapier", fn, assets) 
+return Prefab("sendi_rapier", fn, assets, prefabs) --YUKARI : prefab 어규먼트
